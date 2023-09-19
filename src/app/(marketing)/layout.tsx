046d2705112158076220df3411901b16b759ff4f
mambo-dev <mambo.michael.22@gmail.com>
2023-09-20 00:37:35 +0300

    fix:try adding force dynamic

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -6,6 +6,8 @@ import { Inter } from "next/font/google";
 import { cookies } from "next/headers";
 import { Toaster } from "react-hot-toast";
 
+export const dynamic = "force-dynamic";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
